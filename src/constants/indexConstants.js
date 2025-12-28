e51e7e27920f86a1a6b2d7351f1f518587d87715
Onur DÖKER <odoker@msn.com>
2025-12-28 05:56:44 +0300

    modify backhand

---
+++ src/constants/indexConstants.js
@@ -0,0 +1,5 @@
+export const FIFTEEN_MINUTES = 15 * 60 * 1000;
+export const ONE_DAY = 24 * 60 * 60 * 1000;
+
+export const ACCESS_TOKEN_EXPIRY = "15 minutes";
+export const REFRESH_TOKEN_EXPIRY = "1 day";
